840c9426c8360d859808cb447dad2ba5c63b4c88
mits <mits.jp@gmail.com>
2013-12-14 13:55:30 +0900

    Initial Commit

---
+++ IS-prac6Tests/IS_prac6Tests.h
@@ -0,0 +1,13 @@
+//
+//  IS_prac6Tests.h
+//  IS-prac6Tests
+//
+//  Created by Chida Mitsuhiro on 13/12/14.
+//  Copyright (c) 2013年 Chida Mitsuhiro. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IS_prac6Tests : SenTestCase
+
+@end
---
+++ IS-prac6Tests/IS_prac6Tests.m
@@ -0,0 +1,32 @@
+//
+//  IS_prac6Tests.m
+//  IS-prac6Tests
+//
+//  Created by Chida Mitsuhiro on 13/12/14.
+//  Copyright (c) 2013年 Chida Mitsuhiro. All rights reserved.
+//
+
+#import "IS_prac6Tests.h"
+
+@implementation IS_prac6Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in IS-prac6Tests");
+}
+
+@end
